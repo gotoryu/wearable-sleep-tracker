4e55370df21734a3350eb1e05149b925ebd52a0a
Badi Al-Bahra <al-bahra@campus.tu-berlin.de>
2026-02-22 12:54:53 +0100

    replaced all hardware: esp32 wrover e -> esp32 c3 zero; MPU6050 -> BMI160; HW827 -> MAX30102

--- esp_client/esp_client.ino
+++ esp_client/esp_client.ino
@@ -1,5 +1,5 @@
-#include <Adafruit_MPU6050.h>
-#include <Adafruit_Sensor.h>
+#include <MAX30105.h>
+#include <BMI160Gen.h>
 #include <Wire.h>
 #include <WiFi.h>
 #include <PubSubClient.h>
@@ -10,29 +10,32 @@
 WiFiClient espClient;
 PubSubClient client(espClient);
 long lastMsg = 0;
-#define ledPin 2
-
-Adafruit_MPU6050 mpu;
+#define LED_PIN 10
+#define SDA_PIN 4
+#define SCL_PIN 5
+#define BMI160_I2C_ADDR 0x68
 
 //Pulse Sensor Variables
-#define psPIN 35        // Pulse Sensor PURPLE WIRE connected to ANALOG PIN 0
 int LED = 15;   //  The on-board Arduion LED
 double average_pulse = 0;
 int iterations_pulse = 20;
 int count_p = iterations_pulse;
 int sig;                // holds the incoming raw data. Signal value can range from 0-1024
 
+MAX30105 pulseSensor;
+byte ps_iterations = 4;
+
 unsigned long lastTimeMeasured_movement = millis();
 unsigned long lastTimeMeasured_pulse = millis();
-float averages_motion[] = {0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
-int iterations_motion = 20;
-int count_m = iterations_motion;
+float averages_gyro[] = {0.0, 0.0, 0.0};
+int iterations_gyro = 20;
+int count_m = iterations_gyro;
 
 void blink_led(unsigned int times, unsigned int duration){
   for (int i = 0; i < times; i++) {
-    digitalWrite(ledPin, HIGH);
+    digitalWrite(LED_PIN, HIGH);
     delay(duration);
-    digitalWrite(ledPin, LOW); 
+    digitalWrite(LED_PIN, LOW); 
     delay(200);
   }
 }
@@ -124,25 +127,46 @@ void callback(char* topic, byte* message, unsigned int length) {
 
 void setup(void) {
   Serial.begin(115200);
-  pinMode(ledPin, OUTPUT);
+  pinMode(LED_PIN, OUTPUT);
 
   while (!Serial)
     delay(10); // will pause Zero, Leonardo, etc until serial console opens
 
-  Serial.println("Adafruit MPU6050 test!");
+  Serial.println("BMI160 Test.");
+  Wire.begin(SDA_PIN, SCL_PIN);
 
-  // setup mpu6050 sensor
-  if (!mpu.begin()) {
-    Serial.println("Failed to find MPU6050 chip");
+  // setup BMI160 sensor
+  if (!BMI160.begin(BMI160GenClass::I2C_MODE, BMI160_I2C_ADDR)) {
+    Serial.println("Failed to find BMI160!");
     while (1) {
       delay(10);
     }
   }
-  Serial.println("MPU6050 Found!");
+  Serial.println("BMI160 found!");
 
-  mpu.setAccelerometerRange(MPU6050_RANGE_8_G);
-  mpu.setGyroRange(MPU6050_RANGE_500_DEG);
-  mpu.setFilterBandwidth(MPU6050_BAND_21_HZ);
+  Serial.println("MAX30102 Test.");
+  // setup MAX30102 sensor
+  if (!pulseSensor.begin(Wire, I2C_SPEED_FAST))
+  {
+    Serial.println("Failed to find MAX30105!");
+    while (1) {
+      delay(10);
+    }
+  }
+  Serial.println("MAX30102 Found!");
+
+  byte ledBrightness = 0x1F; //Options: 0=Off to 255=50mA
+  byte sampleAverage = ps_iterations; //Options: 1, 2, 4, 8, 16, 32
+  byte ledMode = 3; //Options: 1 = Red only, 2 = Red + IR, 3 = Red + IR + Green
+  int sampleRate = 100; //Options: 50, 100, 200, 400, 800, 1000, 1600, 3200
+  int pulseWidth = 411; //Options: 69, 118, 215, 411
+  int adcRange = 4096; //Options: 2048, 4096, 8192, 16384
+
+  pulseSensor.setup(ledBrightness, sampleAverage, ledMode, sampleRate, pulseWidth, adcRange);
+
+  // mpu.setAccelerometerRange(MPU6050_RANGE_8_G);
+  // mpu.setGyroRange(MPU6050_RANGE_500_DEG);
+  // mpu.setFilterBandwidth(MPU6050_BAND_21_HZ);
 
   // setup WiFi Connection to MQTT Broker
   setup_wifi();
@@ -155,60 +179,57 @@ void setup(void) {
 
 void handlePulseSensor() {
   unsigned long currentTime = millis();
-  // small delay between data points
-  if (currentTime - lastTimeMeasured_pulse > 0) {
-    lastTimeMeasured_pulse = currentTime;
-    count_p--;
-    sig = analogRead(psPIN);  // Read the PulseSensor's value.
-    average_pulse += sig;
+  int num_samples = pulseSensor.check();
 
-    if (count_p == 0) {
-      // calculate the average of the last 20 values
-      int pulseData = (int)average_pulse / iterations_pulse;
+  if (num_samples > 0) {
+    int pulseDataSum = 0;
+    
+    // STORAGE_SIZE is the size of the ESP32-side buffer (called "sense" in the library).
+    // The Data gets written from the FIFO buffer (MAX30102) to the sense buffer (ESP32). 
+    // So we are basically limiting the number of samples to the size of the sense buffer.
+    int samplesToRead = min(num_samples, STORAGE_SIZE);
 
-      // Format: timestamp,value
-      char message[50];
-      sprintf(message, "%lu,%d", currentTime, pulseData);
-      // send the data to the server on the topic "esp32/pulse"
-      client.publish("esp32/pulse", message);
-
-      Serial.println(message); // Debugging output
-
-      // reset the values
-      average_pulse = 0;
-      count_p = iterations_pulse;
+    for (int i = 0; i < samplesToRead; i++) {
+      pulseDataSum += pulseSensor.getFIFOIR();
+      pulseSensor.nextSample();
     }
+
+    char message[50];
+    sprintf(message, "%lu,%d", currentTime, (int)(pulseDataSum / samplesToRead));
+    client.publish("esp32/pulse", message);
+
+    Serial.println(message);
   }
 }
-
 
 void handleMotionSensor() {
   unsigned long currentTime = millis();
   if (currentTime - lastTimeMeasured_movement > 0) {
     lastTimeMeasured_movement = currentTime;
-    sensors_event_t a, g, temp;
-    mpu.getEvent(&a, &g, &temp);
+    int gx, gy, gz;
+    BMI160.readGyro(gx, gy, gz);
 
-    averages_motion[0] += a.acceleration.x; averages_motion[1] += a.acceleration.y; averages_motion[2] += a.acceleration.z;
-    averages_motion[3] += g.gyro.x; averages_motion[4] += g.gyro.y; averages_motion[5] += g.gyro.z;
+    averages_gyro[0] += gx;
+    averages_gyro[1] += gy;
+    averages_gyro[2] += gz;
 
-    if(count_m == 0) {
-      count_m = iterations_motion;
-      averages_motion[0] /= iterations_motion; averages_motion[1] /= iterations_motion; averages_motion[2] /= iterations_motion;
-      averages_motion[3] /= iterations_motion; averages_motion[4] /= iterations_motion; averages_motion[5] /= iterations_motion;
+    if(count_m == 1) {
+      averages_gyro[0] /= iterations_gyro;
+      averages_gyro[1] /= iterations_gyro;
+      averages_gyro[2] /= iterations_gyro;
 
-      // Format: timestamp,acc_x,acc_y,acc_z,gyro_x,gyro_y,gyro_z
+      // Format: timestamp,gyro_x,gyro_y,gyro_z
       char message[100];
-      sprintf(message, "%lu,%.2f,%.2f,%.2f,%.2f,%.2f,%.2f", currentTime,
-              averages_motion[0], averages_motion[1], averages_motion[2],
-              averages_motion[3], averages_motion[4], averages_motion[5]);
+      sprintf(message, "%lu,%.2f,%.2f,%.2f", currentTime,
+              averages_gyro[0], averages_gyro[1], averages_gyro[2]);
       client.publish("esp32/movement", message);
 
-      Serial.println(message); // Debugging output#
+      Serial.println(message); // Debugging output
 
-      for (int i = 0; i < 6; i++) {
-        averages_motion[i] = 0.0;
-      }
+      averages_gyro[0] = 0.0;
+      averages_gyro[1] = 0.0;
+      averages_gyro[2] = 0.0;
+      count_m = iterations_gyro;
     }
 
     count_m--;
